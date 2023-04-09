4a817d0b0b2a6058ba770207a70a4f5237d221ef
jiaxing gao <79193821+gaoxiaoxin@users.noreply.github.com>
2023-04-09 16:42:28 +0800

    ✨ Feat: 完成了第三方库的引入

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,13 @@
-<template>App</template>
+<template>
+  <el-button>你好</el-button>
+  <div class="box"><span>nihao1</span></div>
+</template>
 <script setup lang="ts"></script>
-<style scoped></style>
+<style scoped lang="scss">
+.box {
+  color: red;
+  span {
+    font-size: 12px;
+  }
+}
+</style>
