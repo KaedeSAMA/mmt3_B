766d0d3a9321a577dd6af5bc987ea81fde8574b8
jiaxing gao <79193821+gaoxiaoxin@users.noreply.github.com>
2023-04-27 01:21:29 +0800

    修改components.d.ts

--- types/components.d.ts
+++ types/components.d.ts
@@ -13,10 +13,7 @@ declare module '@vue/runtime-core' {
     ElCard: typeof import('element-plus/es')['ElCard']
     ElForm: typeof import('element-plus/es')['ElForm']
     ElFormItem: typeof import('element-plus/es')['ElFormItem']
-    ElIcon: typeof import('element-plus/es')['ElIcon']
     ElInput: typeof import('element-plus/es')['ElInput']
-    IEpHare: typeof import('~icons/ep/hare')['default']
-    IEpShare: typeof import('~icons/ep/share')['default']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
   }
