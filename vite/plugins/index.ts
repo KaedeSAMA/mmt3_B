c680782dd07b1821ffa287ef99d38ebf9462d1be
jiaxing gao <79193821+gaoxiaoxin@users.noreply.github.com>
2023-04-27 01:16:15 +0800

    ✨ Feat: 完成了icon图标的自动化导入

--- vite/plugins/index.ts
+++ vite/plugins/index.ts
@@ -1,6 +1,7 @@
 import type { PluginOption } from 'vite';
 import setupAutoImport from './auto-import';
 import setupComponents from './components';
+import setupIcons from './icons';
 import vue from '@vitejs/plugin-vue';
 
 export default function setupVitePlugins() {
@@ -8,7 +9,8 @@ export default function setupVitePlugins() {
     vue(),
     // 自动引入插件
     setupAutoImport(),
-    setupComponents()
+    setupComponents(),
+    setupIcons()
   ];
   return plugins;
 }
