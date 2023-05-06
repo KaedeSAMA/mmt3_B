cd87a50ab62479d9b27a97b8787ca87b9de78049
gaoxiaoxin <gaojiaxing@kkworld.com>
2023-05-06 21:25:44 +0800

    ✨ Feat: 完成了头部导航栏和项目大体架构的搭建

--- src/pages/Home/home.vue
+++ src/pages/Home/home.vue
@@ -1,32 +1,29 @@
 <template>
   <div class="home">
     <el-container>
-      <el-aside width="200px">Aside</el-aside>
+      <el-aside width="200px"><Aside /> </el-aside>
       <el-container>
-        <el-header>Header</el-header>
+        <el-header><Head /></el-header>
         <el-main>Main</el-main>
         <el-footer>Footer</el-footer>
       </el-container>
     </el-container>
   </div>
 </template>
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import Aside from './components/aside/index.vue';
+import Head from './components/head/index.vue';
+</script>
 <style scoped lang="scss">
 .home {
   height: 100%;
   .el-container {
     height: 100%;
-    .el-aside {
-      background-color: aqua;
-    }
-    .el-header {
-      background-color: red;
-    }
     .el-main {
-      background-color: blue;
+      border: 1px solid blue;
     }
     .el-footer {
-      background-color: green;
+      border: 1px solid #333;
     }
   }
 }
