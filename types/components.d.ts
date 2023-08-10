7759688b61dbed8d31422f5703a64981707d528b
argrule <2039858744@qq.com>
2023-08-10 10:19:43 +0800

    :heavy_plus_sign: fix el-plus icon

--- types/components.d.ts
+++ types/components.d.ts
@@ -17,7 +17,6 @@ declare module '@vue/runtime-core' {
     ElContainer: typeof import('element-plus/es')['ElContainer']
     ElDialog: typeof import('element-plus/es')['ElDialog']
     ElDivider: typeof import('element-plus/es')['ElDivider']
-    ElFooter: typeof import('element-plus/es')['ElFooter']
     ElForm: typeof import('element-plus/es')['ElForm']
     ElFormItem: typeof import('element-plus/es')['ElFormItem']
     ElHeader: typeof import('element-plus/es')['ElHeader']
