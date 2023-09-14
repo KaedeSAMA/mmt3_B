4315142a767d099f181f04615a3e317b04f7fe4a
argrule <2039858744@qq.com>
2023-09-14 09:43:13 +0800

    :ambulance: Fix: 面试看板页面使用axios未经过拦截器

--- src/api/dateBoard/index.ts
+++ src/api/dateBoard/index.ts
@@ -1,6 +1,6 @@
 import { _axios } from '@/server/http';
-import axios from 'axios';
-import { BASE_URL, TIME_OUT } from '@/server/request/config';
+// import axios from 'axios';
+// import { BASE_URL, TIME_OUT } from '@/server/request/config';
 import showErrorInfo from '@/server/request/error';
 import {
   TGetDateBoardMessageRes,
@@ -16,21 +16,26 @@ import {
 } from '@/api/dateBoard/types/paramsType';
 // 全部数据，既有query又有body原生axios
 const getFilterData = async (page: Tpage, filterCondition: Tfilter) => {
-  const res = await axios({
-    url: BASE_URL + '/local/b/data/dashboard/sift',
-    timeout: TIME_OUT,
-    method: 'post',
-    headers: {
-      'Content-Type': 'application/json',
-      Authorization: localStorage.getItem('token')
-    },
-    params: page,
-    data: filterCondition
-  });
-  if (res.status != 200) {
+  // const res = await axios({
+  //   url: BASE_URL + '/local/b/data/dashboard/sift',
+  //   timeout: TIME_OUT,
+  //   method: 'post',
+  //   headers: {
+  //     'Content-Type': 'application/json',
+  //     Authorization: localStorage.getItem('token')
+  //   },
+  //   params: page,
+  //   data: filterCondition
+  // });
+  const res = await _axios.post<TGetDateBoardMessageRes, Tfilter>(
+    `/local/b/data/dashboard/sift?page=${page.page}&pageNum=${page.pageNum}`,
+    filterCondition
+  );
+
+  if (res.code != '00000') {
     showErrorInfo({ response: res });
   }
-  return res.data.data;
+  return res.data!;
 };
 //导出报名表的接口
 const getExportData = async (filterCondition: Tfilter) => {
